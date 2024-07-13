a87bcd2d24f3558bc287978b21d9b7193131fab7
KaiLiu12 <74182067+onetrickollie@users.noreply.github.com>
2024-07-12 21:06:20 -0700

    now shows connected, disconnected, and typing activity

--- server/index.js
+++ server/index.js
@@ -12,24 +12,42 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, "public")))
 
-const expressServer = app.listen(PORT,() =>{
+const expressServer = app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
 
-const io = new Server(expressServer,{
+const io = new Server(expressServer, {
     // limiting access
     cors: {
-        origin: process.env.NODE_ENV === "production" ? false : 
-        ["http://127.0.0.1:5500","http://localhost:5500"]
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
     }
 })
 
 io.on('connection', socket => {
     console.log(`User ${socket.id} connected`)
 
+    // Upon connection - only to user
+    // hence socket.emit, not io.emit
+    // io.emit will send to everyone, socket.emit will send to specific socket(user)
+    socket.emit('message', "Welcome to Chat App!")
+
+    // Upon connection - to everyone connected
+    socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)} connected`)
+
+    // Listening for a message event
     socket.on('message', data => {
         console.log(data)
-        //sending messgae and userid back to log; (0,5) is the first 5 chacs of the string
-        io.emit('message',`${socket.id.substring(0,5)}:${data}`)
+        //sending message and userid back to log; (0,5) is the first 5 chars of the string
+        io.emit('message', `${socket.id.substring(0, 5)}: ${data}`)
+    })
+
+    // when users disconnects - to all others
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('message', `User ${socket.id.substring(0, 5)}} disconnected`)
+    })
+
+    // display message when typing
+    socket.on('activity', (name) => {
+        socket.broadcast.emit('activity', name)
     })
 })
--- server/public/app.js
+++ server/public/app.js
@@ -1,15 +1,17 @@
 const socket = io('ws://localhost:3500')
 
+const activity = document.querySelector('.activity')
+const msgInput = document.querySelector('input')
+
 function sendMessage(e) {
     e.preventDefault()
-    const input = document.querySelector('input')
 // if input is valid
 // message sent and reset
-    if (input.value) {
-        socket.emit('message', input.value)
-        input.value = ""
+    if (msgInput.value) {
+        socket.emit('message', msgInput.value)
+        msgInput.value = ""
     }
-    input.focus()
+    msgInput.focus()
 }
 // Button
 
@@ -18,7 +20,26 @@ document.querySelector('form')
 
 // Listen for messages 
 socket.on("message", (data) => {
+    activity.textContent = ""
     const li = document.createElement('li')
     li.textContent = data
     document.querySelector('ul').appendChild(li)
+})
+
+msgInput.addEventListener('keypress',() =>{
+    socket.emit('activity',socket.id.substring(0,5))
+})
+
+
+//show typing activity
+let activityTimer
+socket.on("activity",(name)=>{
+    activity.textContent = `${name} is typing...`
+
+    // clear after 1.5 seconds
+    clearTimeout(activityTimer)
+    activityTimer = setTimeout(() =>{
+        activity.textContent = ""
+    },3000)
+
 })
