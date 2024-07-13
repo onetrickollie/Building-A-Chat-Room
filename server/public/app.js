c758e8b63935f1a8d4869152172580a64cc2301a
KaiLiu12 <74182067+onetrickollie@users.noreply.github.com>
2024-07-12 21:50:32 -0700

    clientside js updated

--- server/public/app.js
+++ server/public/app.js
@@ -1,23 +1,45 @@
 const socket = io('ws://localhost:3500')
 
+const msgInput = document.querySelector('#message')
+const nameInput = document.querySelector('#name')
+const chatRoom = document.querySelector('#room')
 const activity = document.querySelector('.activity')
-const msgInput = document.querySelector('input')
+const userList = document.querySelector('.user-list')
+const roomList = document.querySelector('.room-list')
+const chatDisplay = document.querySelector('.chat-display')
 
 function sendMessage(e) {
     e.preventDefault()
 // if input is valid
 // message sent and reset
-    if (msgInput.value) {
-        socket.emit('message', msgInput.value)
+    if (nameInput.value && msgInput.value && chatRoom.value) {
+        socket.emit('message', {
+            "name":nameInput.value,
+            "text":msgInput.value
+        })
         msgInput.value = ""
     }
     msgInput.focus()
 }
+
+function enterRoom(e){
+    e.preventDefault()
+    if(nameInput.value && chatRoom.value){
+        socket.emit('enterRoom',{
+            "name": nameInput.value,
+            "room": chatRoom.value
+        })
+    }
+}
+
 // Button
-
-document.querySelector('form')
+document.querySelector('.form-msg')
     .addEventListener('submit', sendMessage)
-
+document.querySelector('.form-join')
+    .addEventListener('submit', enterRoom)
+    msgInput.addEventListener('keypress',() =>{
+        socket.emit('activity',socket.id.substring(0,5))
+    })
 // Listen for messages 
 socket.on("message", (data) => {
     activity.textContent = ""
@@ -25,11 +47,6 @@ socket.on("message", (data) => {
     li.textContent = data
     document.querySelector('ul').appendChild(li)
 })
-
-msgInput.addEventListener('keypress',() =>{
-    socket.emit('activity',socket.id.substring(0,5))
-})
-
 
 //show typing activity
 let activityTimer
