048416fdd1eeb08c3dda47cd62e5d83a955aaa51
KaiLiu12 <74182067+onetrickollie@users.noreply.github.com>
2024-07-12 18:22:52 -0700

    sockio server side update

--- server/index.js
+++ server/index.js
@@ -1,10 +1,25 @@
-const ws = require('ws')
-const server = new ws.Server({ port: '3000' })
+import { createserver} from "http"
+import {server} from "socket.io"
 
-server.on('connection', socket => {
-    socket.on('message', message => {
-        const b = Buffer.from(message)
-        console.log(b.toString())
-        socket.send(`${message}`)
+const httpServer = createServer()
+
+const io = new Server(httpServer,{
+    // limiting access
+    cors: {
+        origin: process.env.NODE_ENV === "production" ? false : 
+        ["http://localhost:5500"]
+    }
+})
+
+io.on('connection', socket => {
+    console.log(`User ${socket.id} connected`)
+
+    socket.on('message', data => {
+        console.log(data)
+        //sending messgae and userid back to log; (0,5) is the first 5 chacs of the string
+        io.emit('message',`${socket.id.substring(0,5)}:${data}`)
     })
 })
+
+
+httpServer.listen(3500,() => console.log('listening on port 3500'))
