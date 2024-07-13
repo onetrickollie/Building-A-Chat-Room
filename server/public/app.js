c32f9119690343e688f5898e0474b015aa1d218b
KaiLiu12 <74182067+onetrickollie@users.noreply.github.com>
2024-07-12 23:56:24 -0700

    app updated for onrender deployment

--- server/public/app.js
+++ server/public/app.js
@@ -1,4 +1,4 @@
-const socket = io('https://chatroom430.netlify.app/')
+const socket = io('https://chatroom420.onrender.com')
 
 const msgInput = document.querySelector('#message')
 const nameInput = document.querySelector('#name')
